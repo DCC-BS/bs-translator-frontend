e7da23a48e97d5c3dd7faa0e866d554772790f51
Tobias Bollinger <tobias.bollinger@bs.ch>
2025-09-23 13:49:22 +0200

    removed the kanto bs styles

--- app/app.vue
+++ app/app.vue
@@ -1,13 +1,8 @@
 <script lang="ts" setup>
-import { Disclaimer } from "@dcc-bs/common-ui.bs.js";
 </script>
 
 <template>
     <UApp>
-        <button class="button is-link">
-            <icon-symbol-add aria-hidden="true"></icon-symbol-add>
-            Click me!
-        </button>
         <NuxtLayout>
             <NuxtPage />
         </NuxtLayout>
